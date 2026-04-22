9ad4f406dd08997bc28cc18e7935bf83924bee22
hkbae <hb3135@nyu.edu>
2026-04-22 15:08:28 -0400

    setup for nyu hpc

--- scripts/active_matter/run_train_jepa.sh
+++ scripts/active_matter/run_train_jepa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source "$(dirname "$0")/../env_setup.sh"
+# source "$(dirname "$0")/../env_setup.sh"
 
-torchrun --nproc_per_node=8 --standalone \
+torchrun --nproc_per_node=2 --standalone \
     -m physics_jepa.train_jepa \
     configs/train_activematter_small.yaml \
     $1
