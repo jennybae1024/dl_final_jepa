bb34665365de77adf59fa35f59dc25eaaabd12b7
Hyunkyung Bae <jennybae1024@gmail.com>
2026-04-22 23:34:11 -0400

    jepa conv tiny vit

---
+++ scripts/active_matter/run_train_vit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# source "$(dirname "$0")/../env_setup.sh"
+
+torchrun --nproc_per_node=2 --standalone \
+    -m physics_jepa.train_jepa \
+    configs/train_activematter_small.yaml \
+    model=conv_vit_tiny \
+    $1
